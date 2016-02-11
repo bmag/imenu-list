a68d596b437ce1c125d8bd5414467ca1ff55bdcc
bmag <bmagamb@gmail.com>
2016-02-11 13:41:54 +0200

    Bump version

--- imenu-list.el
+++ imenu-list.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2015 Bar Magal
 
 ;; Author: Bar Magal (2015)
-;; Version: 0.4
+;; Version: 0.5
 ;; Homepage: https://github.com/bmag/imenu-list
 ;; Package-Requires: ((cl-lib "0.5"))
 
